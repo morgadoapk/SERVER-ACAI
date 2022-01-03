092130f2a4ba9950b93708ff9df21a31c27495f8
riancarlosdev <riancontatoprofissional@gmail.com>
2022-01-02 21:56:25 -0300

    push

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    const connect = await mongoose.connect(`mongodb+srv://admin:86042781sa@sistema.jensb.mongodb.net/sistema?retryWrites=true&w=majority`)
-    return connect
+    await mongoose.connect(`mongodb+srv://admin:86042781sa@sistema.jensb.mongodb.net/sistema?retryWrites=true&w=majority`)
 }
 
 module.exports = { main }
